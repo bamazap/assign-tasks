17d98fd2864e2bdcacd5f20d27c83610508d028e
Barry Anthony McNamara III <bamazap@hotmail.com>
2017-11-19 21:48:12 -0500

    Can increase or decrease penalty for high rank.

--- assign_tasks.py
+++ assign_tasks.py
@@ -6,12 +6,14 @@ from tabulate import tabulate
 parser = ArgumentParser(description="Assign tasks to people.")
 
 parser.add_argument("csvfile", type=str, help="Path to CSV file.")
+parser.add_argument("-r", "--ratio", type=float, default=1,
+        help="Scale ranking i to i*geom(r,i)")
 
 args = parser.parse_args()
 
 names = []
 tasks = []
-data = []
+rankings = []
 with open(args.csvfile) as f:
     first_row = True
     for line in f.readlines():
@@ -21,14 +23,17 @@ with open(args.csvfile) as f:
             first_row = False
         else:
             names.append(row[0])
-            data.append(list(map(lambda i: float(i), row[1:])))
+            rankings.append(list(map(lambda r: float(r), row[1:])))
 
-data = np.array(data)
-row_ind, col_ind = linear_sum_assignment(data)
+costs = np.array(rankings)
+if args.ratio != 1:
+    costs = args.ratio**-1 * (1 - np.power(args.ratio, costs))/(1 - args.ratio)
+print(costs)
+row_ind, col_ind = linear_sum_assignment(costs)
 
 assignments = []
 for i, r in enumerate(row_ind):
     c = col_ind[i]
-    assignments.append([names[r], tasks[c], data[r][c]])
+    assignments.append([names[r], tasks[c], rankings[r][c]])
 
 print(tabulate(assignments, headers=["Name", "Task", "Rank"]))
